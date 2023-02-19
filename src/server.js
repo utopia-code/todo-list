a469b29b46e655fa4a8e2257b3243bb6f0086a9c
utopia-code <utopiacode.developer@gmail.com>
2023-02-19 21:07:55 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const expressStaticGzip = require('express-static-gzip');
+const app = express();
+
+
+app.get('/', function (req,res) {
+    const absolutePathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+    const contentsOfHtmlFile = fs.readFileSync(absolutePathToHtmlFile, 'utf-8');
+    res.send(contentsOfHtmlFile);
+});
+
+app.use('/static', expressStaticGzip(
+    path.resolve(__dirname, '../dist'), {
+        enableBrotli: true,
+        orderPreference: ['br', 'gz']
+    }
+));
+
+app.listen(3000, function() {
+    console.log('Appliation is running on http://localhost:3000');
+});
